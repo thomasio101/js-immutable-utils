24c1d13364754f1e86616dbc531e17c0e17bd7d0
Thomas Egbert Duursma <thomasduursma@outlook.com>
2019-11-03 16:39:58 +0100

    Add set functions

---
+++ src/set/index.ts
@@ -0,0 +1,12 @@
+export function add<T>(set: Set<T>, value: T): Set<T> {
+    return new Set(set).add(value)
+}
+
+export function remove<T>(set: Set<T>, value: T): { removed: boolean, set: Set<T> } {
+    const newSet = new Set(set)
+
+    return {
+        removed: newSet.delete(value),
+        set: newSet
+    }
+}
