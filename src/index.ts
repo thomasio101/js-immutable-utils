96638c1394ffd65dee3d3041cc9bcb275700dfc5
Thomas Egbert Duursma <thomasduursma@outlook.com>
2019-11-03 17:14:49 +0100

    Export the set module from the main file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as _array from './array';
 import * as _object from './object';
+import * as _set from './set';
 
 export const array = _array;
 export const object = _object;
+export const set = _set;
